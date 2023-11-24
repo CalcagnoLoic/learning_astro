aed371df6c5cedc00edd8ffc172a2a6c837f14c5
Loïc Calcagno <calcagnoloic93@gmail.com>
2023-11-24 13:12:22 +0100

    ajoux d'un flux rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,11 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
-export async function get() {
+export async function GET() {
     return rss({
         title: 'Astro Learner | Blog',
         description: 'Mon voyage d\'apprentissage d\'Astro',
         site: 'https://CalcagnoLoic.github.io/learning_astro',
         items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-        customData: `<language>en-en</language>`,
+        customData: `<language>en-us</language>`,
     });
 }
