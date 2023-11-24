6de0b2d1d8efac398b8c7f846a12ced7bd5aa0b2
Loïc Calcagno <calcagnoloic93@gmail.com>
2023-11-24 14:04:16 +0100

    ajout d'un dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://calcagnoloic.github.io/learning_astro/'
+  site: 'https://calcagnoloic.github.io/learning_astro/',
+  integrations: [preact()]
 });
